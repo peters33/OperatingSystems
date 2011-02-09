0fa281868f6c46975ebdbe848afe7e3ed40ad439
Andrew Petersen <peters33@uwm.edu>
2011-02-08 21:57:18 -0600

    the wait functions are working.  Only big thing left is to implement the execvp

--- lab2/runsim.cpp
+++ lab2/runsim.cpp
@@ -10,7 +10,8 @@ int main (int argc, char *argv[])
 	ifstream inFile;
 	int pr_count = 0;
 	int pr_limit = atoi(argv[1]);
-	
+	pid_t childpid = 0;
+
 	inFile.open("testsim.data");
 	if (!inFile)
 	{
@@ -25,27 +26,57 @@ int main (int argc, char *argv[])
 		{
 			//TODO: Implement waiting stuff
 			// Decrement count
-			fprintf(stderr, "Reached the limit, pr_count=%i, pr_limit=%i \n", pr_count, pr_limit);
+			fprintf(stderr, "\nReached the limit, pr_count=%i, pr_limit=%i \n", pr_count, pr_limit);
 			wait(NULL);
 			pr_count--;
+			fprintf(stderr, "A child process finished\n");			
 		}
 		
 		inFile.getline (command, 256);
 		
-		// TODO: Execute command on a forked process 
-		//instead of displaying it
+		// Execute command on a forked process 
 		// (fork, makeargv, execvp)
-		cout << command << endl;
-		
+
+		fprintf(stderr, "The command line is : ");
+		fprintf(stderr, command);
+		fprintf(stderr, "Current pr_count = %i, pr_limit = %i \n", pr_count, pr_limit);
 		pr_count++;
-		
-		//TODO: check to see if any children have finished
+		childpid = fork();    
+		if(childpid == -1) 
+		{
+			printf("Failed to fork process.");
+			return 0;
+		}
+
+		if (childpid == 0)
+		{
+			//RUN THE "command" figure out how to get
+			// argument list
+			const char* program = "testsim";
+			char* args[2];
+			args[0] = "3";
+			args[1] = "2";
+			//execvp(program,args);
+			return 0;
+		}
+
+		//check to see if any children have finished
 		// Decrement pr_count foreach completed child;
+		int status;
+		while(waitpid(0, &status, WNOHANG) != 0)
+		{
+			pr_count--;
+		}
 	}
+
 	inFile.close();	
 	
 	//TODO: Wait for the rest of the children to finish
+	while(wait(NULL) > 0) ;
 
 	return 0;
 }
+
+
+
 
