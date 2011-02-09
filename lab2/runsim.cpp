0966e4ed2a5074fb2132c72ffe175eda7642f2b7
Andrew Petersen <peters33@uwm.edu>
2011-02-08 20:35:56 -0600

    added runsim.cpp

---
+++ lab2/runsim.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fstream>
+
+using namespace std;
+
+int main (int argc, char *argv[]) 
+{
+	ifstream inFile;
+	int pr_count = 0;
+	int pr_limit = atoi(argv[1]);
+	
+	inFile.open("testsim.data");
+	if (!inFile)
+	{
+		cerr << "Unable to open testsim.data";
+		exit(1);
+	}
+	
+	char command[256];
+	while (! inFile.eof() )
+	{
+		if (pr_count == pr_limit)
+		{
+			//TODO: Implement waiting stuff
+			// Decrement count
+		}
+		
+		inFile.getline (command, 256);
+		
+		// TODO: Execute command on a forked process 
+		//instead of displaying it
+		// (fork, makeargv, execvp)
+		cout << command << endl;
+		
+		pr_count++;
+		
+		//TODO: check to see if any children have finished
+		// Decrement pr_count foreach completed child;
+	}
+	inFile.close();	
+	
+	//TODO: Wait for the rest of the children to finish
+
+	return 0;
+}
+
