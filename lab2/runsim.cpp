3031ae9c8666ff4b1ce4263775a22bd7225a74ae
John Seaman <jtseaman85@gmail.com>
2011-02-10 15:02:28 -0600

    reverted to old c++ version
    Signed-off-by: John Seaman <jtseaman85@gmail.com>

--- lab2/runsim.cpp
+++ lab2/runsim.cpp
@@ -2,88 +2,75 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <fstream>
-#include <stdio.h>
-#include <errno.h>
-#include <sys/wait.h>
 
 using namespace std;
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-	FILE* inFile;
+	ifstream inFile;
 	int pr_count = 0;
 	int pr_limit = atoi(argv[1]);
 	pid_t childpid = 0;
-	char cmdLineStr[256];
-	int status;
-	fpos_t pos;
 
-	if (pr_limit == 0)
+	inFile.open("testsim.data");
+	if (!inFile)
 	{
-		perror ("Process limit must be greater than 0. Exiting...");
+		cerr << "Unable to open testsim.data";
+		exit(1);
 	}
 
-	inFile = fopen("testsim.data", "r");
-	if (inFile == NULL)
+	char command[256];
+	while (! inFile.eof() )
 	{
-		perror ("Error opening file. Exiting...");
-	}
-	else 
-	{
-		while (fgets(cmdLineStr, 256, inFile) != NULL)
+		if (pr_count == pr_limit)
 		{
-			// Store current position in file.
-			fgetpos(inFile, &pos);
-			
-			// Ignore blank lines in file
-			if(!strcmp(cmdLineStr, "\n"))
-			{
-				continue;
-			}
-		
-			if (pr_count == pr_limit)
-			{
-				fprintf(stderr, "Reached process limit, waiting...\n");
-				
-				// Wait for next running process to terminate
-				wait(NULL);
-				pr_count--;		
-			}
-			
-			fprintf(stderr, "Current process count = %i, process limit = %i\n", pr_count, pr_limit);
-
-			if((childpid = fork()) < 0) 
-			{
-				fprintf(stderr, "Failed to fork process.\n");
-				return 1;
-			}
-
-			pr_count++;
-
-			if (childpid == 0)
-			{
-				// Execute command
-				system(cmdLineStr);
-				return 0;
-			}
-
-			//check to see if any children have finished
-			// Decrement pr_count foreach completed child;
-			while(waitpid(0, &status, WNOHANG) != 0)
-			{
-				pr_count--;
-			}
+			//TODO: Implement waiting stuff
+			// Decrement count
+			fprintf(stderr, "\nReached the limit, pr_count=%i, pr_limit=%i \n", pr_count, pr_limit);
+			wait(NULL);
+			pr_count--;
+			fprintf(stderr, "A child process finished\n");
 		}
 
-		fclose(inFile);
-		
-		//TODO: Wait for the rest of the children to finish
-		while(wait(NULL) > 0) ;
+		inFile.getline (command, 256);
+
+		// Execute command on a forked process
+		// (fork, makeargv, execvp)
+
+		fprintf(stderr, "The command line is : ");
+		fprintf(stderr, command);
+		fprintf(stderr, "Current pr_count = %i, pr_limit = %i \n", pr_count, pr_limit);
+		pr_count++;
+		childpid = fork();
+		if(childpid == -1)
+		{
+			printf("Failed to fork process.");
+			return 0;
+		}
+
+		if (childpid == 0)
+		{
+			//RUN THE "command" figure out how to get
+			// argument list
+			//execvp(program,args);
+			system(command);
+			return 0;
+		}
+
+		//check to see if any children have finished
+		// Decrement pr_count foreach completed child;
+		int status;
+		while(waitpid(0, &status, WNOHANG) != 0)
+		{
+			pr_count--;
+		}
 	}
+
+	inFile.close();
+
+	//TODO: Wait for the rest of the children to finish
+	while(wait(NULL) > 0) ;
 
 	return 0;
 }
-
-
-
 
