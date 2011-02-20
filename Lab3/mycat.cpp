d193fd6bb8841b644ca6f9403ea41f1fc6871d64
John Seaman <jtseaman@uwm.edu>
2011-02-19 19:51:36 -0600

    initial

--- Lab3/mycat.cpp
+++ Lab3/mycat.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	ifstream inFile;
+	char* filename;
 	
+	for (int i = 1; i < argc; i++)						// Iterate through all command line args
+	{
+		
+		inFile.open(argv[i]);
+		if (!inFile)
+		{
+			cerr << "Unable to open ";
+			cerr << argv[i];
+			exit(1);
+		}
+		
+		inFile.close();
+	}
 	return 0;
 }
