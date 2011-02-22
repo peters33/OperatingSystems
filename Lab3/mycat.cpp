8e67558560ffa85c3d7c2efa9e1e7ccd08e38e57
Andrew Petersen <peters33@uwm.edu>
2011-02-21 22:25:15 -0600

    added if statement to account for blank input

--- Lab3/mycat.cpp
+++ Lab3/mycat.cpp
@@ -9,7 +9,7 @@ void GetFileStream(char* filename);
 void GetStandarInput();
 void CopyOutputStreamToOutputFile();
 
-ostream outputstream;
+//WHAT DO WE STORE EVERYTHING (input filestreams and standard input) IN UNTIL WE ARE DONE?
 
 int main(int argc, char *argv[])
 {
@@ -19,30 +19,41 @@ int main(int argc, char *argv[])
 	bool isNumbered = false;
 	char* outputFile;
 	
+	if (argc == 3 && (strcmp(argv[i],"@") == 0))
+	{
+		//WE HAVE A SPECIAL BLANK SCENARIO
+		// cat > outputfile.txt
+		// aka mycat @ outfile.txt
+	}
+
 	// Iterate through all command line args
 	for (int i = 1; i < argc; i++)
 	{
 		fileStr.clear();
 		
+		// -N
 		if (strcmp(argv[i],"-n") == 0) 
 		{
 			isNumbered = true;			
 		} 
+		// -
 		if (strcmp(argv[i],"-") == 0)
 		{
 			GetStandarInput();
 		}
+		// @ outputfilename
 		if (strcmp(argv[i],"@") == 0)
 		{
 			i++;
 			outputFile = argv[i];
 		}
+		// filename
 		else
 		{
 			GetFileStream(argv[i])
 		}
 	}
-	CopyOutputStreamToOutputFile(outputFile);
+	CopyOutputStreamToOutputFile(outputFile); // if output file is null, then output is standard out
 	return 0;
 }
 
@@ -61,14 +72,14 @@ void GetFileStream(filename)
 		while (!fileStr.eof()) {
 			fileStr.getline(buffer, sizeof(buffer));
 
-			if (isNumbered) {
-				outputstream << lineNum << " " << buffer << endl;
-				//	printf("%i\t%s\n", lineNum, buffer);
-				//lineNum++;
-			} else {
-				outputstream << buffer << endl;
-				//printf("%s\n", buffer);
-			}
+			//if (isNumbered) {
+			//	outputstream << lineNum << " " << buffer << endl;
+			//	//	printf("%i\t%s\n", lineNum, buffer);
+			//	//lineNum++;
+			//} else {
+			//	outputstream << buffer << endl;
+			//	//printf("%s\n", buffer);
+			//}
 		}
 		// ****************** End Read File ************************
 	}
@@ -78,7 +89,7 @@ void GetFileStream(filename)
 void CopyOutputStreamToOutputFile(char* filename)
 {
 	/* Create an fstream based on the output file name
-	and copy the outputstream to the output fstream
+	and copy the all the stored input to the output fstream
 	*/
 }
 
