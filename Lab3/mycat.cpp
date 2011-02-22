ccc0b5c664f7d1584a42a5992d3d86df6816e448
Andrew Petersen <peters33@uwm.edu>
2011-02-22 14:51:12 -0600

    added functionality to CopyInputToOutputStream

--- Lab3/mycat.cpp
+++ Lab3/mycat.cpp
@@ -1,25 +1,37 @@
+// myCat.cpp : Defines the entry point for the console application.
+//
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
 #include <string.h>
+#include <string>
 
 using namespace std;
 
-void GetFileStream(char* filename);
-void GetStandarInput();
-void CopyOutputStreamToOutputFile();
 
-//WHAT DO WE STORE EVERYTHING (input filestreams and standard input) IN UNTIL WE ARE DONE?
+/*
+	CopyInputToOutputStream works and copies inputString to the specified output
+
+	need to implement GetFileStream to take an input file and put it into InputString
+	same for GetStandardInput
+
+*/ 
+void GetFileStream(char* filename, bool numbered);
+//void GetStandarInput();
+void CopyInputToOutputStream(char* outputStreamName);
+
+string inputString;
+fstream fileStr;
 
 int main(int argc, char *argv[])
-{
-	fstream fileStr;
+{	
 	int lineNum;
 	char buffer[50];
 	bool isNumbered = false;
-	char* outputFile;
-	
-	if (argc == 3 && (strcmp(argv[i],"@") == 0))
+	char* outputFile = "standard";
+	inputString = "Test input bla bla bla";
+
+	if (argc == 3 && (strcmp(argv[1],"@") == 0))
 	{
 		//WE HAVE A SPECIAL BLANK SCENARIO
 		// cat > outputfile.txt
@@ -39,7 +51,7 @@ int main(int argc, char *argv[])
 		// -
 		if (strcmp(argv[i],"-") == 0)
 		{
-			GetStandarInput();
+			//GetStandarInput();
 		}
 		// @ outputfilename
 		if (strcmp(argv[i],"@") == 0)
@@ -50,15 +62,16 @@ int main(int argc, char *argv[])
 		// filename
 		else
 		{
-			GetFileStream(argv[i])
+			GetFileStream(argv[i], isNumbered);
 		}
 	}
-	CopyOutputStreamToOutputFile(outputFile); // if output file is null, then output is standard out
+	CopyInputToOutputStream(outputFile); // if output file is null, then output is standard out
 	return 0;
 }
 
-void GetFileStream(filename)
+void GetFileStream(char* filename, bool numbered)
 {
+
 	fileStr.open(filename);
 
 	if (fileStr.fail()) {
@@ -66,12 +79,18 @@ void GetFileStream(filename)
 		fileStr.clear();
 
 	} else {
-		lineNum = 1;
+		int	lineNum = 1;
 
 		// ****************** Begin Read File **********************
 		while (!fileStr.eof()) {
-			fileStr.getline(buffer, sizeof(buffer));
-
+			if (numbered)
+			{
+				inputString += lineNum;
+				lineNum++;
+			}			
+			getline(fileStr,inputString);
+			
+			//fileStr.getline(buffer, sizeof(buffer));
 			//if (isNumbered) {
 			//	outputstream << lineNum << " " << buffer << endl;
 			//	//	printf("%i\t%s\n", lineNum, buffer);
@@ -86,16 +105,36 @@ void GetFileStream(filename)
 	fileStr.close();
 }
 
-void CopyOutputStreamToOutputFile(char* filename)
+void CopyInputToOutputStream(char* outputStream)
 {
 	/* Create an fstream based on the output file name
 	and copy the all the stored input to the output fstream
 	*/
+	if (strcmp(outputStream,"standard") == 0)
+	{
+		cout << inputString;
+	}
+	else
+	{
+		fileStr.clear();
+		fileStr.open(outputStream);
+
+		if (fileStr.fail()) 
+		{
+			// open failed, output error message
+			fileStr.clear();
+		}
+
+		fileStr << inputString;
+		fileStr.close();
+	}
 }
 
-void GetStandarInput()
-{
-	Add standard input until ctrl-D
-		Loop Get-Line Calls until control D is found
-}
+//void GetStandarInput()
+//{
+//	Add standard input until ctrl-D
+//		Loop Get-Line Calls until control D is found
+//}
+
+
 
